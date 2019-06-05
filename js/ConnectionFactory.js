155f8388ffbb76630e16c213ac16cb71367b788e
Heiko Henning UE85540 <heiko.henning@sbb.ch>
2019-06-05 14:32:24 +0200

    Update queue stuff

---
+++ js/ConnectionFactory.js
@@ -0,0 +1,4 @@
+document.getElementById("hosturl").value = "ws://shared-rcssolace-node01.otc-test.sbb.ch/";
+document.getElementById("message-vpn").value = "pingu-VPN";
+document.getElementById("username").value = "default";
+document.getElementById("password").value = "default";
